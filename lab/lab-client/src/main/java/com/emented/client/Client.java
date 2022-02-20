06a3bf398d4fdc3453bb717b1b0a059a7604e587
Ilya <ilerys61@gmail.com>
2022-02-20 22:53:36 +0300

    Added exception if file not found

--- lab/lab-client/src/main/java/com/emented/client/Client.java
+++ lab/lab-client/src/main/java/com/emented/client/Client.java
@@ -4,6 +4,7 @@ import com.emented.client.commandLine.CommandListener;
 import com.emented.client.entities.CollectionOfMusicBands;
 import com.emented.client.parser.XMLParser;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public final class Client {
@@ -13,8 +14,12 @@ public final class Client {
 
     public static void main(String[] args) throws IOException {
         XMLParser r = new XMLParser();
-        CollectionOfMusicBands cc = r.readFromXML("MusicBands.xml");
-        CommandListener cl = new CommandListener(cc);
-        cl.readCommandsFromConsole();
+        try {
+            CollectionOfMusicBands cc = r.readFromXML(args[0]);
+            CommandListener cl = new CommandListener(cc);
+            cl.readCommandsFromConsole();
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
     }
 }
